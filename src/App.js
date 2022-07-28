9641a2fd54e9229de41d7a7d1d1daae6bc1c308b
h1692 <hualf@dtdream.com>
2022-07-28 10:57:21 +0800

    feat:atom & selector demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { 
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+import { RecoilRoot } from 'recoil';
+import CharacterCounter from './components/CharacterCounter';
 import './App.css';
 
 function App() {
   return (
     <RecoilRoot>
       <div className="App">
-        <h1>Recoil todo list</h1>
-        
+        <h1>Recoil todo list demo</h1>
+        <CharacterCounter />
       </div>
     </RecoilRoot>
   );
--- src/components/CharacterCounter.js
+++ src/components/CharacterCounter.js
@@ -1,22 +1,53 @@
 import {
   useRecoilState,
+  useRecoilValue,
   selector,
+  atom,
 } from 'recoil';
-const  CharacterCounter =()=>(
+
+// atom对象
+const textState = atom({
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: '', // default value (aka initial value)
+});
+
+// selector pure function
+const charCountState = selector({
+  key:'charCountState',
+  get:({get})=>{
+    const text = get(textState);
+    return text.length;
+  }
+});
+
+// mian
+const CharacterCounter = () => (
   <div>
     <TextInput />
+    <CharacterCount />
   </div>
 )
 
- const TextInput = ()=>{
-  
+// component 1
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+  return <>Character Count is: {count}</>;
+}
+
+// component 2
+const TextInput = () => {
+  // 获取atom数据
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
   return (
     <div>
-      <input />
+      <input type="text" value={text} onChange={onChange} />
       <br />
-      Echo:{}
+      Echo is:{text}
     </div>
   )
- }
+}
 
-export default CharacterCounter
+export default CharacterCounter;
